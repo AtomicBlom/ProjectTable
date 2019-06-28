37c6dbdeb1c77c0c20e2bc38e617b1399570bc11
Steven Blom <atomicblom@binaryvibrance.net>
2019-06-28 22:33:52 +0930

    Make it compile again

--- src/main/java/com/github/atomicblom/projecttable/events/WorldEvents.java
+++ src/main/java/com/github/atomicblom/projecttable/events/WorldEvents.java
@@ -1,12 +1,13 @@
 package com.github.atomicblom.projecttable.events;
 
-import com.github.atomicblom.projecttable.ProjectTableMod;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
-@SubscribeEvent
+@Mod.EventBusSubscriber
 public class WorldEvents {
+    @SubscribeEvent
     public static void onClientJoinedWorld(PlayerEvent.PlayerLoggedInEvent event) {
-        ProjectTableMod.network.sendTo(new message, event.player);
+        //ProjectTableMod.network.sendTo(new message, event.player);
     }
 }
